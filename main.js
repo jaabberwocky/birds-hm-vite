af1867477bcb299b12c317d748b810cafd04e28f
tobias <tobias@tobias-leong.dev>
2023-02-12 11:54:30 +0800

    Change audio eventListeners

--- main.js
+++ main.js
@@ -4,13 +4,24 @@ import { TOBIAS } from './_base';
 const myCanvas = document.querySelector('#my-canvas');
 const typewriterTarget = document.querySelector('#my-canvas .text-box .typewriter')
 const audio = new Audio('./o.mp3');
+let audioPlaying = false;
 
 audio.volume = 0.5;
-audio.autoplay = true;
 audio.loop = true;
 
 window.addEventListener('click', () => {
-  audio.play();
+  checkAudio();
+})
+
+window.addEventListener('mousedown', function (e) { e.preventDefault(); }, false);
+
+window.addEventListener('scroll', (e) => e.preventDefault())
+
+window.addEventListener('touchmove', (e) => { checkAudio(); e.preventDefault(); }, { passive: false })
+
+window.addEventListener('touchend', () => checkAudio())
+
+window.addEventListener('dblclick', (e) => {
   if (!document.fullscreenElement) {
     myCanvas.requestFullscreen();
   } else {
@@ -18,13 +29,21 @@ window.addEventListener('click', () => {
   }
 })
 
-window.addEventListener('mousedown', function (e) { e.preventDefault(); }, false);
+window.addEventListener('keypress', (e) => {
+  if (e.code == 'Space') {
+    if (!audio.paused) {
+      audio.pause();
+    } else {
+      audio.play();
+    }
+  }
+})
 
-window.addEventListener('scroll', (e) => e.preventDefault())
-
-window.addEventListener('touchmove', (e) => { audio.play(); e.preventDefault(); }, { passive: false })
-
-window.addEventListener('touchend', () => audio.play())
+const checkAudio = () => {
+  if (audio.paused) {
+    audio.play()
+  }
+}
 
 
 const typer = new Typewriter(typewriterTarget, {
